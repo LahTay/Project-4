c696ec21447a969c730c8c57090e56010ba6047d
tymek2906 <66008176+tymek2906@users.noreply.github.com>
2020-06-01 22:22:27 +0200

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
+
+#include <windows.h>
+#include <gdiplus.h>
+#include <tchar.h>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <deque>
+#include <cmath>
+#include <vector>
+#include <mbstring.h>
+#include <vector>
+#pragma comment(lib,"gdiplus.lib")
+
+using namespace Gdiplus;
